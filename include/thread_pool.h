1c122afe2398457f93ebc794e093b91e58ed047d
keletso <keletsomonyamane.dev@gmail.com>
2026-06-01 15:18:09 +0200

    feat: Add threadpool header class and woritem struct

---
+++ include/thread_pool.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <functional>
+#include <atomic>
+#include <string>
+
+struct WorkItem{
+    int client_fp;
+    std::string client_ip;
+};
+
+class ThreadPool {
+public:
+    explicit ThreadPool(size_t num_thread, size_t max_queue_size);
+    ~ThreadPool();   
+    bool enqueue(WorkItem item);
+    void stop();
+    void set_handler(std::function<void(WorkItem)> handler);
+    
+private:
+    std:: vector<std::thread>  workers_;
+    std::queue<WorkItem>   queue_;
+    std::mutex   mutex_;
+    std::condition_variable  cv_;
+    std::atomic<bool>  stopping_{ false};
+    size_t  max_queue_size_t_;
+    std::function<void(WorkItem)> hnandler;
+
+    void worker_loop();
+};
