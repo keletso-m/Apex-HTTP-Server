93bc3c71386671180ffc37b4a08d594714f9bffd
keletso <keletsomonyamane.dev@gmail.com>
2026-06-21 23:58:40 +0200

    feat: add public get_prefix implementation

--- src/core/router.cpp
+++ src/core/router.cpp
@@ -17,3 +17,6 @@ void Router::post(const std::string& path, RouteHandler handler) {
 void Router::any(const std::string& path, RouteHandler handler) {
     add_route("", path, false, std::move(handler));
 }
+void Router::get_prefix(const std::string& path, RouteHandler handler) {
+    add_route("GET", path, true, std::move(handler));
+}
