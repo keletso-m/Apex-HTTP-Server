ffeb048398a4b2cbfcc4d7c38b47a53596e65553
keletso <keletsomonyamane.dev@gmail.com>
2026-06-21 23:20:57 +0200

    chore: register prefix routes

--- include/router.h
+++ include/router.h
@@ -20,3 +20,5 @@ public:
     void get(const std::string& path, RouteHandler handler);
     void post(const std::string& path, RouteHandler handler);
     void any(const std::string& path, RouteHandler handler);
+    // register prfix routes lke "/" cattches eevertyhing 
+    void get_prefix(const std::string& path, RouteHandler handler);
